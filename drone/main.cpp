c2f77f6fb0190826bedd7b99894ca77eb3ba5b75
grooke <49820048+grookee@users.noreply.github.com>
2025-10-11 13:10:01 +0000

    drone level1

---
+++ drone/main.cpp
@@ -0,0 +1,22 @@
+/**
+▪ In this level the drone can only fly up or down
+▪ The time of a flight is measured in ticks
+▪ For every tick you are given the velocity at that time
+▪ If the drone is flying up, the velocity is positive
+▪ If the drone is flying down, the velocity is negative
+▪ Every tick, the drone’s height will be updated based on the current velocity
+▪ At the beginning, the drone is at height 0
+▪ The height will never be negative during the flight
+▪ The drone took multiple flights
+▪ For each flight calculate the drone's final height
+
+input is in level1_1.in
+*/
+
+#include <fstream>
+
+int main() {
+
+    return 0;
+}
+
